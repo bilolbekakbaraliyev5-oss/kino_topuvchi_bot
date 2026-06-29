7bd53f20688d8c8824ebe6937540c7fa4568d8bb
bilolbekakbaraliyev5-oss <bilolbekakbaraliyev5@gmail.com>
2026-06-29 21:22:11 +0500

    Update kino_bot.py

--- kino_bot.py
+++ kino_bot.py
@@ -1,3 +1,18 @@
+from flask import Flask
+from threading import Thread
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Bot is running"
+
+def run():
+    app.run(host='0.0.0.0', port=10000)
+
+# Buni kodning eng oxiriga yoki asosiy qismiga qo'shing:
+t = Thread(target=run)
+t.start()
 import os
 from flask import Flask
 from threading import Thread
